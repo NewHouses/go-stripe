c267a42c0ce5b7bdc871cd87993733b0aa460a90
NewHouses <sergio.simons.c@gmail.com>
2023-10-31 18:19:32 +0100

    fixing print format

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -44,7 +44,7 @@ func (app *application) serve() error {
 		WriteTimeout:      5 * time.Second,
 	}
 
-	app.infoLog.Println("Starting HTTP server in {} on port %d", app.config.env, app.config.port)
+	app.infoLog.Printf("Starting HTTP server in %s on port %d", app.config.env, app.config.port)
 	return srv.ListenAndServe()
 }
 
