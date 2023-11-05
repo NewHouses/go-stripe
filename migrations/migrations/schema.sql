289caae630b3c10217c595c807508b4c6d6ca6de
NewHouses <sergio.simons.c@gmail.com>
2023-11-05 12:06:14 +0100

    Creating database models

--- migrations/migrations/schema.sql
+++ migrations/migrations/schema.sql
@@ -143,6 +143,7 @@ CREATE TABLE `widgets` (
   `price` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `image` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -156,4 +157,4 @@ CREATE TABLE `widgets` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-05 11:51:00
+-- Dump completed on 2023-11-05 12:04:44
