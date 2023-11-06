a9b6650efee250cc3b5eed7db6a199b70dac52eb
NewHouses <sergio.simons.c@gmail.com>
2023-11-06 17:39:42 +0100

    Setting up subscriptions

--- internal/models/model.go
+++ internal/models/model.go
@@ -54,6 +54,8 @@ type Widget struct {
 	InventoryLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
 	Image          string    `json:"image"`
+	IsRecurring    bool      `json:"is_recurring"`
+	PlanId         string    `json:"plan_id"`
 	CreatedAt      time.Time `json:"-"`
 	UpdatedAt      time.Time `json:"-"`
 }
@@ -111,7 +113,7 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 
 	row := m.DB.QueryRowContext(ctx, `
 		select 
-			id, name, description, inventory_level, price, image, created_at, updated_at
+			id, name, description, inventory_level, price, image, is_recurring, plan_id, created_at, updated_at
 		from
 			widgets
 		where id = ?`, id)
@@ -122,6 +124,8 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 		&widget.InventoryLevel,
 		&widget.Price,
 		&widget.Image,
+		&widget.IsRecurring,
+		&widget.PlanId,
 		&widget.CreatedAt,
 		&widget.UpdatedAt,
 	)
