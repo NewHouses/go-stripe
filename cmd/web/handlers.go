ad8e3f469e1616dfb5e3421a0d7d1fb6af7ac8a9
NewHouses <sergio.simons.c@gmail.com>
2023-11-16 15:22:56 +0100

    Setting up stub pages for sales and subscriptions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -372,3 +372,15 @@ func (app *application) ShowResetPassoword(w http.ResponseWriter, r *http.Reques
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) AllSales(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "all-sales", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+func (app *application) AllSubscriptions(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "all-subscriptions", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
